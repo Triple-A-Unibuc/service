d2d0dc85d8cf1c510c1a5690a87b73cd6a624302
nicugnm <nghergu17@gmail.com>
2024-03-12 20:20:06 +0200

    final refactor + finish auth (refesh token added too)

--- src/main/java/ro/tripleaunibuc/domain/auth/exception/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package ro.tripleaunibuc.domain.auth.exception;
-
-public class AuthenticationException extends RuntimeException {
-
-	public AuthenticationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-}

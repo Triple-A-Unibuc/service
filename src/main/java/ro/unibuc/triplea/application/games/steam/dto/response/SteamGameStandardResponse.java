edc2c94230bba23c6535082d95261ba2fedd2733
Bănilean Alexandru-Ioan <91990185+alexbanilean@users.noreply.github.com>
2024-03-19 12:46:29 +0000

    fix: type change

--- src/main/java/ro/unibuc/triplea/application/games/steam/dto/response/SteamGameStandardResponse.java
+++ src/main/java/ro/unibuc/triplea/application/games/steam/dto/response/SteamGameStandardResponse.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 public class SteamGameStandardResponse {
     private String code;
     private String message;
-    private Object data;
+    private String data;
 }

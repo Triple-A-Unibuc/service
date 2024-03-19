db8113fc52b92594e7f6e0463f25b8010a75b6b8
RaduADumitru <radu.dumitru@rocketmail.com>
2024-03-19 19:27:37 +0200

    feat(reviews): add skeleton

---
+++ src/main/java/ro/unibuc/triplea/domain/reviews/steam/repository/SteamGameReviewRepository.java
@@ -0,0 +1,17 @@
+package ro.unibuc.triplea.domain.reviews.steam.repository;
+
+import ro.unibuc.triplea.application.reviews.steam.dto.response.SteamGameReviewResponse;
+import ro.unibuc.triplea.domain.reviews.steam.model.entity.SteamGameReview;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SteamGameReviewRepository {
+    Optional<List<SteamGameReviewResponse>> findAllByEmail(String email);
+    Optional<List<SteamGameReviewResponse>> findAllByGameSteamId(int gameSteamId);
+    Optional<List<SteamGameReviewResponse>> findAllByGameName(String gameName);
+    Optional<List<SteamGameReviewResponse>> findAllByUserName(String userName);
+
+    SteamGameReview save(SteamGameReview game);
+
+}
---
+++ src/main/java/ro/unibuc/triplea/domain/reviews/steam/service/SteamGameReviewService.java
@@ -0,0 +1,7 @@
+package ro.unibuc.triplea.domain.reviews.steam.service;
+
+import ro.unibuc.triplea.domain.reviews.steam.model.entity.SteamGameReview;
+
+public class SteamGameReviewService {
+
+}

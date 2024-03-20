3cecdcc6ee82cddac39ed7e9edf8c75b56967cb3
RaduADumitru <radu.dumitru@rocketmail.com>
2024-03-20 10:41:20 +0200

    feat(reviews): add complete review functionality

--- src/main/java/ro/unibuc/triplea/application/reviews/steam/web/SteamGameReviewController.java
+++ src/main/java/ro/unibuc/triplea/application/reviews/steam/web/SteamGameReviewController.java
@@ -1,4 +1,52 @@
 package ro.unibuc.triplea.application.reviews.steam.web;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.unibuc.triplea.application.reviews.steam.dto.request.SteamGameReviewRequest;
+import ro.unibuc.triplea.application.reviews.steam.dto.response.SteamGameReviewResponse;
+import ro.unibuc.triplea.domain.reviews.steam.model.entity.SteamGameReview;
+import ro.unibuc.triplea.domain.reviews.steam.service.SteamGameReviewService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/reviews/steam")
+@RequiredArgsConstructor
+@Tag(name = "SteamGameReviews", description = "Steam Game Review management APIs")
 public class SteamGameReviewController {
+
+    private final SteamGameReviewService steamGameReviewService;
+    @GetMapping("/game-id/{gameId}")
+    public ResponseEntity<?> getReviewsBySteamId(@PathVariable String gameId) {
+        Optional<List<SteamGameReviewResponse>> reviews = steamGameReviewService.getReviewsByGameIdentifier(gameId);
+        if (reviews.isPresent()) {
+            return ResponseEntity.ok(reviews.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/user-id/{userId}")
+    public ResponseEntity<?> getReviewsByUserId(@PathVariable String userId) {
+        Optional<List<SteamGameReviewResponse>> reviews = steamGameReviewService.getReviewsByUser(userId);
+        if (reviews.isPresent()) {
+            return ResponseEntity.ok(reviews.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addReview(@RequestBody SteamGameReviewRequest review) {
+        Optional<SteamGameReviewResponse> addedReview = steamGameReviewService.addReview(review);
+        if (addedReview.isPresent()) {
+            return ResponseEntity.ok(addedReview.get());
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 }

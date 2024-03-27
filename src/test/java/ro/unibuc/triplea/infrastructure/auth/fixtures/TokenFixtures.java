cc624eb667dd420cfebca94de10e5cc7779b21f0
nicugnm <nghergu17@gmail.com>
2024-03-27 22:40:36 +0200

    feat(testing-auth): add auth unit testing for all classes

---
+++ src/test/java/ro/unibuc/triplea/infrastructure/auth/fixtures/TokenFixtures.java
@@ -0,0 +1,15 @@
+package ro.unibuc.triplea.infrastructure.auth.fixtures;
+
+import ro.unibuc.triplea.domain.auth.model.entity.meta.Token;
+import ro.unibuc.triplea.domain.auth.model.enums.TokenType;
+
+public class TokenFixtures {
+
+    public static Token revokedNotExpiredToken(String token) {
+        return new Token(1, token, TokenType.BEARER, true, false, UserFixtures.user("username"));
+    }
+
+    public static Token validToken(String token) {
+        return new Token(1, token, TokenType.BEARER, false, false, UserFixtures.user("username"));
+    }
+}
